0bb78f9cebd929a81eee46b3e72d41e1ca647c18
C. J. Anderson <chris@standsure.io>
2023-07-29 08:04:16 +0530

    text code sample

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp;
+
+public static partial class Program
+{
+    private static void Main(string[] args)
+    {
+        HelloFrom("Generated Code");
+        Console.WriteLine("end");
+    }
+
+    static partial void HelloFrom(string name);
+}
